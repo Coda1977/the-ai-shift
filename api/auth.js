4c9231a0da0d653584ba5d877848a305e1f53fc7
Coda1977 <Coda1977@users.noreply.github.com>
2026-02-01 14:52:35 +0200

    Debug auth function with try/catch

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,13 @@
 module.exports = function handler(req, res) {
-  var clientId = process.env.OAUTH_GITHUB_CLIENT_ID;
-  var scope = 'repo,user';
-  var authURL = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&scope=' + scope;
-  res.setHeader('Location', authURL);
-  res.status(302).end();
+  try {
+    var clientId = process.env.OAUTH_GITHUB_CLIENT_ID;
+    var scope = 'repo,user';
+    var authURL = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&scope=' + scope;
+    res.setHeader('Location', authURL);
+    res.statusCode = 302;
+    res.end();
+  } catch (e) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: e.message, stack: e.stack }));
+  }
 };
